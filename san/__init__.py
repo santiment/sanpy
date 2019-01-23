c5eb645ce026a16bcb9647c0e5f4f4aed76ecbf4
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-01-23 16:08:33 +0200

    Add warning outdated version

--- san/__init__.py
+++ san/__init__.py
@@ -1,3 +1,20 @@
 from .get import get
 from .batch import Batch
 from .api_config import ApiConfig
+import pkg_resources
+import requests
+import json
+from warnings import warn
+
+def get_latest(project):
+    url = 'https://pypi.python.org/pypi/%s/json' % (project)
+    response = requests.get(url).text
+    return json.loads(response)['info']['version']
+
+project = 'sanpy'
+current_version = pkg_resources.require(project)[0].version
+latest = get_latest(project)
+
+if current_version != latest:
+    warning = 'The package %s is out of date. Your version is %s, the latest is %s.' % (project, current_version, latest)
+    warn(warning)
