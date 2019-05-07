8c7cf3b30f610402380e1d5d3c99000faa9d53e2
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-05-07 10:25:04 +0300

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sanpy",
-    version="0.4.3",
+    version="0.5.0",
     author="Santiment",
     author_email="admin@santiment.net",
     description="Package for Santiment API access with python",
