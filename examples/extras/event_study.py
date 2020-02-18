3a480e578cedd7006f65b6d58f4999d639ff0382
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2020-02-18 14:26:01 +0200

    Clean-up event_study example

--- examples/extras/event_study.py
+++ examples/extras/event_study.py
@@ -8,21 +8,21 @@ sys.path.append("../..")
 from san.extras.event_study import event_study, signals_format
 
 data = san.get("ohlcv/bitcoin")
-data["BTC_close"] = data.closePriceUsd
+data["bitcoin"] = data.closePriceUsd
 data["ETH_close"] = san.get("ohlcv/ethereum").closePriceUsd
 data["daily_active_addresses_ETH"] = san.get("daily_active_addresses/ethereum")
-
 
 data["daa_performance"] = np.log(data["daily_active_addresses_ETH"].pct_change() + 1)
 data["sd_rolling"] = data["daa_performance"].rolling(100).std()
 
-price = data[["ETH_close", "BTC_close"]]
-price.columns = ['ETH_close','bitcoin']
+price = data[["ETH_close", "bitcoin"]]
 price=price.tz_convert(None)  # Event Study needs date instead of datetime
 
-
-data["buy"] = (data["daa_performance"] < (data["sd_rolling"] * (-1) * 2))  # Buy Signal: Is the current trend lower than two standard deviations?
-data["sell"] = (data["daa_performance"] > (data["sd_rolling"] * 2))  # Sell Signal: Is the current trend higher than two standard deviations?
+# Defining signals:
+# Buy Signal: Is the current trend lower than two standard deviations?
+data["buy"] = (data["daa_performance"] < (data["sd_rolling"] * (-1) * 2))
+# Sell Signal: Is the current trend higher than two standard deviations?
+data["sell"] = (data["daa_performance"] > (data["sd_rolling"] * 2))
 
 
 # Helper function to get the signals in the right format:
