862986dbd1501c267e7589e312ff31d88eeb9a6d
JanSmirny <42940622+JanSmirny@users.noreply.github.com>
2020-03-09 16:48:44 +0100

    event study bug fixed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sanpy",
-    version="0.7.16",
+    version="0.7.17",
     author="Santiment",
     author_email="admin@santiment.net",
     description="Package for Santiment API access with python",
