e4583b2bb673898a9b8a9e963d7c26199fa859f4
spiderjako <lyudmil_danailov@abv.bg>
2020-07-13 11:11:51 +0300

    Adding available since to sanpy

--- san/available_metrics.py
+++ san/available_metrics.py
@@ -20,3 +20,17 @@ def available_metrics_for_slug(slug):
     )
 
     return execute_gql(query_str)['projectBySlug']['availableMetrics']
+
+
+def available_metric_for_slug_since(metric, slug):
+    query_str = ("""{{
+        getMetric(metric: \"{metric}\"){{
+            availableSince(slug: \"{slug}\")
+        }}
+    }}
+    """).format(
+        metric=metric,
+        slug=slug
+    )
+
+    return execute_gql(query_str)['getMetric']['availableSince']
