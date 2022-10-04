d1382b5eb38d0eb451468670e487d2a08b29e516
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-10-04 10:58:19 +0300

    Remove outdated version warning

--- san/__init__.py
+++ san/__init__.py
@@ -20,17 +20,3 @@ def get_latest():
         return json.loads(response)['info']['version']
     except requests.exceptions.RequestException as e:
         return pkg_resources.get_distribution(PROJECT).version
-
-
-def warn_if_outdated():
-    current_version = pkg_resources.get_distribution(PROJECT).version
-    latest_version = get_latest()
-
-    if current_version != latest_version:
-        warning = 'The package %s is out of date. Your version is %s, the latest is %s.' % (
-            PROJECT, current_version, latest_version)
-        warn(warning)
-        pass
-
-
-warn_if_outdated()
