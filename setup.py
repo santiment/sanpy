4597bb6ab94dbe598544df720df3ae440c9ea823
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2023-01-28 22:12:34 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sanpy',
-    version='0.11.3',
+    version='0.11.4',
     author='Santiment',
     author_email='admin@santiment.net',
     description='Package for Santiment API access with python',
