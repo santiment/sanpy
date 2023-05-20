266a265b602acb3e55bc667f3d2b3a385b2208bb
Artur.Dossatayev <artur.dossatayev@coins.ph>
2023-05-20 18:53:45 +0600

    refactor available_metrics.py

--- san/available_metrics.py
+++ san/available_metrics.py
@@ -2,35 +2,57 @@ import inspect
 import san.sanbase_graphql
 from san.graphql import execute_gql
 
-def available_metrics():
-    sanbase_graphql_functions = inspect.getmembers(san.sanbase_graphql, inspect.isfunction)
-    all_functions =  list(map(lambda x: x[0], sanbase_graphql_functions)) + execute_gql('{query: getAvailableMetrics}')['query']
-    all_functions = list(filter(lambda x: not (str.startswith(x, "get_metric") or str.startswith(x, "_")), all_functions))
+
+def available_metrics() -> list:
+    """
+    Returns a list of all available metrics
+    :param: None
+    :return: list of all available metrics
+    """
+    sanbase_graphql_functions = inspect.getmembers(
+        san.sanbase_graphql, inspect.isfunction
+    )
+    all_functions = (
+        list(map(lambda x: x[0], sanbase_graphql_functions))
+        + execute_gql("{query: getAvailableMetrics}")["query"]
+    )
+    all_functions = list(
+        filter(
+            lambda x: not (str.startswith(x, "get_metric") or str.startswith(x, "_")),
+            all_functions,
+        )
+    )
     return all_functions
 
 
-def available_metrics_for_slug(slug):
-    query_str = ("""{{
+def available_metrics_for_slug(slug: str) -> list:
+    """
+    Returns a list of all available metrics for a given slug
+    :param slug: slug of the project
+    :return: list of all available metrics for a given slug
+
+    """
+    query_str = f"""{{
         projectBySlug(slug: \"{slug}\"){{
             availableMetrics
         }}
     }}
-    """).format(
-        slug=slug
-    )
-
-    return execute_gql(query_str)['projectBySlug']['availableMetrics']
+    """
+    return execute_gql(query_str)["projectBySlug"]["availableMetrics"]
 
 
-def available_metric_for_slug_since(metric, slug):
-    query_str = ("""{{
+def available_metric_for_slug_since(metric: str, slug: str) -> str:
+    """
+    Returns the date since a metric is available for a given slug
+    :param metric: metric name
+    :param slug: slug of the project
+    :return: datetime since a metric is available for a given slug
+    """
+    query_str = f"""{{
         getMetric(metric: \"{metric}\"){{
             availableSince(slug: \"{slug}\")
         }}
     }}
-    """).format(
-        metric=metric,
-        slug=slug
-    )
+    """
 
-    return execute_gql(query_str)['getMetric']['availableSince']
+    return execute_gql(query_str)["getMetric"]["availableSince"]
